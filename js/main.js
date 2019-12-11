d55ff0e524d2d0ffabce3f96b003dc567105314d
Gianluca Pericoli <gianluca.pericoli@interlogica.it>
2019-12-11 13:34:16 +0100

    q

---
+++ js/main.js
@@ -0,0 +1,39 @@
+window.onload = () => {
+    'use strict';
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register('./sw.js');
+    }
+    document.getElementById("income").value = localStorage.getItem('i');
+    document.getElementById("time").value = localStorage.getItem('t');
+}
+
+function doMagic(){
+    if(income.value != "" && time.value != "" && price.value != ""){
+        let i = parseFloat(income.value);
+        let t = parseFloat(time.value);
+        let p = parseFloat(price.value);
+
+        obj = {
+            'i': i,
+            't': t
+        }
+        localStorage.setItem('i', i);
+        localStorage.setItem('t', t);
+
+        let cost_per_hour = i/t;
+        let hours_worked = p/cost_per_hour;
+
+        str = "You worked "+ Math.ceil(hours_worked) + " hours for this!"
+        txt.innerHTML = str;
+    }
+}
+
+let income = document.getElementById("income");
+let time = document.getElementById("time");
+let price = document.getElementById("price");
+let txt = document.getElementById("txt");
+
+income.addEventListener("keyup", doMagic());
+time.addEventListener("keyup", doMagic);
+price.addEventListener("keyup", doMagic);
---
+++ sw.js
@@ -0,0 +1,23 @@
+var cacheName = 'poor-pwa';
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/css/style.css',
+    '/js/main.js'
+];
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+});
